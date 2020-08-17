53d9977532f1cc4084ed7a541e51492c6ea2886c
Anwesh43 <anweshthecool0@gmail.com>
2020-08-18 02:05:54 +0530

    adding constants and libraries

--- birotlineshooterview/src/main/java/com/anwesh/uiprojects/birotlineshooterview/BiRotLineShooterView.kt
+++ birotlineshooterview/src/main/java/com/anwesh/uiprojects/birotlineshooterview/BiRotLineShooterView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.birotlineshooterview
 /**
  * Created by anweshmishra on 18/08/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<String> = arrayOf("#3F51B5", "#009688", "#F44336", "#4CAF50", "#03A9F4")
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val sizeFactor : Float = 5.4f
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val rot : Float = 45f
+val backColor : Int = Color.parseColor("#BDBDBD")
