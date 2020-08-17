a1833eb9506755f6b675c022c6831f762a8275fb
Anwesh43 <anweshthecool0@gmail.com>
2020-08-18 02:35:39 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedbirotlineshooterview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbirotlineshooterview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedbirotlineshooterview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.birotlineshooterview.BiRotLineShooterView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiRotLineShooterView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
