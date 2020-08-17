7b3f1d97faa3639dc30981f6eedebe623827d583
Anwesh43 <anweshthecool0@gmail.com>
2020-08-18 02:17:35 +0530

    adding BiRotLineShooterView class

--- birotlineshooterview/src/main/java/com/anwesh/uiprojects/birotlineshooterview/BiRotLineShooterView.kt
+++ birotlineshooterview/src/main/java/com/anwesh/uiprojects/birotlineshooterview/BiRotLineShooterView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawBRLSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiRotLineShooter(scale, w, h, paint)
 }
+
+class BiRotLineShooterView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
