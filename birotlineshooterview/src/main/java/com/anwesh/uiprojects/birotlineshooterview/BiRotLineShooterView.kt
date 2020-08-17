aabd066fd25d0aa6c10b0da4ac0939f24afbdade
Anwesh43 <anweshthecool0@gmail.com>
2020-08-18 02:15:56 +0530

    adding Canvas extension method

--- birotlineshooterview/src/main/java/com/anwesh/uiprojects/birotlineshooterview/BiRotLineShooterView.kt
+++ birotlineshooterview/src/main/java/com/anwesh/uiprojects/birotlineshooterview/BiRotLineShooterView.kt
@@ -25,3 +25,34 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBiRotLineShooter(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        rotate(rot * sf3)
+        drawLine(0f, 0f, size * sf1, 0f, paint)
+        restore()
+    }
+    save()
+    translate(0f, -(h / 2 - size) * sf3)
+    drawLine(0f, 0f, 0f, -size * sf2, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawBRLSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiRotLineShooter(scale, w, h, paint)
+}
